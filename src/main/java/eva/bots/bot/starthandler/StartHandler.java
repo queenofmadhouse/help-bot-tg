d031e7fd860b7af349e9ec6afa39a5f77040d9c2
Evgenia Chendeva <evgenia.chendeva@proton.me>
2024-07-09 17:23:57 +0300

    start message update

--- src/main/java/eva/bots/bot/starthandler/StartHandler.java
+++ src/main/java/eva/bots/bot/starthandler/StartHandler.java
@@ -1,5 +1,6 @@
 package eva.bots.bot.starthandler;
 
+import eva.bots.bot.mainmenu.MainMenuButtonsHandler;
 import eva.bots.dto.TelegramMessageDTO;
 import eva.bots.exception.TelegramRuntimeException;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,7 @@ import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -19,17 +20,23 @@ import java.util.List;
 @Slf4j
 public class StartHandler {
 
+    private final MainMenuButtonsHandler mainMenuButtonsHandler;
+
     public List<TelegramMessageDTO> handleMainMenu(Update update) {
 
         if (update.getMessage() != null &&
                 update.getMessage().getText().equals("/start")){
-            return provideStartMessage(update.getMessage());
+            return provideStartMessage(update);
         }
 
         throw new TelegramRuntimeException("Can't handle start menu");
     }
 
-    private List<TelegramMessageDTO> provideStartMessage(Message message) {
+    private List<TelegramMessageDTO> provideStartMessage(Update update) {
+
+        Message message = update.getMessage();
+
+        List<TelegramMessageDTO>  startMessages = new ArrayList<>();
 
         SendMessage messageWithText = new SendMessage();
         SendSticker sticker = new SendSticker();
@@ -37,9 +44,18 @@ public class StartHandler {
         String text = "Привет! \uD83D\uDC4B \n" +
                 "Я Мурзик с сайта Трансляции, теперь я могу проконсультировать вас и через этот бот! \n" +
                 "\n" +
-                "✔\uFE0FОбращайтесь ко мне за информационной консультацией по любым вопросам, с которыми работает Трансляция (медицинская помощь/ психологи/ сообщество / услуги других специалистов / эмиграция / группы поддержки / терапевтическая группа / просвещение / юристы и др.) \n" +
+                "✔\uFE0FОбращайтесь ко мне за *информационной консультацией* по любым вопросам, с которыми работает Трансляция (" +
+                "[медицинская помощь](https://translyaciya.com/doctors) / " +
+                "[психологи](https://translyaciya.com/therapy) / " +
+                "[сообщество](https://translyaciya.com/cities) / " +
+                "[услуги других специалистов](https://translyaciya.com/others) / " +
+                "[эмиграция](https://translyaciya.com/emigration) / " +
+                "[группы поддержки](https://translyaciya.com/transhelp) / " +
+                "[терапевтическая группа](https://translyaciya.com/group) / " +
+                "[просвещение](https://translyaciya.com/materials) / " +
+                "[юристы](https://translyaciya.com/lawyers) и др.) \n" +
                 "\n" +
-                "\uD83D\uDCCCЕсли вы попали в экстренную ситуацию, то нажмите кнопку \uD83C\uDD98 И я свяжусь с вами как можно скорее и передам вашу заявку специалистам. \n" +
+                "\uD83D\uDCCCЕсли вы попали в *экстренную ситуацию*, то нажмите кнопку \uD83C\uDD98 И я свяжусь с вами как можно скорее и передам вашу заявку специалистам. \n" +
                 "\n" +
                 "Экстренная ситуация - это положение, которое угрожает вашей жизни, здоровью или безопасности. (Например: нападение, донос, пришедшая повестка и др.) \n" +
                 "\n" +
@@ -56,6 +72,12 @@ public class StartHandler {
                 .sendSticker(sticker)
                 .build();
 
-        return Collections.singletonList(telegramMessageDTO);
+        startMessages.add(telegramMessageDTO);
+
+        mainMenuButtonsHandler.handleStartMessage(message).forEach(menu -> startMessages.add(TelegramMessageDTO.builder()
+                .sendMessage(menu)
+                .build()));
+
+        return startMessages;
     }
 }
